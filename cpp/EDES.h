e46d481229b362e0b844671dbb18e37e6f448152
diogoftm <dftm@ua.pt>
2023-10-03 19:55:12 +0100

    Add basic EDES algorithm & test main

---
+++ cpp/EDES.h
@@ -0,0 +1,15 @@
+#ifndef DES_FUNCTIONS_H
+#define DES_FUNCTIONS_H
+
+#include <cstdint>
+#include <algorithm> 
+
+namespace EDES {
+    // public methods
+    uint8_t* encrypt(uint8_t* in, uint32_t inSize);
+    
+
+        
+};
+
+#endif // DES_FUNCTIONS_H
