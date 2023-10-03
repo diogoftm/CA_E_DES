9d00a70ae6971f3b62820aa0d175250c7461aaa7
Tiago Silvestre <tiago.silvestre@ua.pt>
2023-10-03 23:21:56 +0100

    updated speed.cpp

--- cpp/speed.cpp
+++ cpp/speed.cpp
@@ -7,10 +7,13 @@
 #include <unistd.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
-
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include "EDES.h"
+#include <openssl/rand.h>
+#include <openssl/conf.h>
 
-#define TRIES 5
+#define TRIES 5000
 #define TEST_DATA_SIZE 4096
 #define UNSET_NS -1
 
@@ -30,38 +33,36 @@ void fill_byte_arr_randomly(uint8_t arr[], unsigned int arr_length)
 
 Statistics test_DES()
 {
+    OpenSSL_add_all_algorithms();
 
     Statistics statistics;
 
     static uint8_t data[TEST_DATA_SIZE];
-    static uint8_t key[DES_KEY_SZ];
+    static DES_cblock key;
     static uint8_t out[TEST_DATA_SIZE];
-    int out_len;
+    static DES_key_schedule keysched;
 
     timespec beforeEncT, afterEncT;
 
-    OPENSSL_add_all_algorithms_conf();
-
-    for (unsigned int i = 0; i < TEST_DATA_SIZE; i++)
+    for (unsigned int i = 0; i < TRIES; i++)
     {
         fill_byte_arr_randomly(data, sizeof(data));
-
-        EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-
-        EVP_EncryptInit_ex(ctx, EVP_des_cfb(), NULL, key, NULL);
+        DES_random_key(&key);
+        DES_set_key((DES_cblock *)key, &keysched);
 
         clock_gettime(CLOCK_MONOTONIC, &beforeEncT);
 
-        EVP_EncryptUpdate(ctx, out, &out_len, data, sizeof(data));
-        EVP_EncryptFinal_ex(ctx, out + out_len, &out_len);
 
+        DES_ecb_encrypt((DES_cblock *)data, (DES_cblock *)out, &keysched, DES_ENCRYPT);
+
+        
         clock_gettime(CLOCK_MONOTONIC, &afterEncT);
+
         long long elapsed_ns = ELAPSED_NS(beforeEncT, afterEncT);
 
         if (statistics.lowest_time_ns == UNSET_NS || elapsed_ns < statistics.lowest_time_ns)
             statistics.lowest_time_ns = elapsed_ns;
 
-        EVP_CIPHER_CTX_free(ctx);
     }
 
     return statistics;
@@ -74,9 +75,9 @@ Statistics test_EDES()
     timespec beforeEncT, afterEncT;
 
     static uint8_t data[TEST_DATA_SIZE];
+    //static uint8_t out[TEST_DATA_SIZE];
 
-    static uint8_t out[TEST_DATA_SIZE];
-    int out_len;
+    //int out_len;
 
     for (unsigned int i = 0; i < TRIES; i++)
     {
@@ -107,4 +108,7 @@ int main(int argc, char *argv[])
 
     fprintf(stdout, " DES fastest encryption time: %lldns\n", DES_Stats.lowest_time_ns);
     fprintf(stdout, "EDES fastest encryption time: %lldns\n", EDES_Stats.lowest_time_ns);
+
+    long long des_slowness = EDES_Stats.lowest_time_ns / DES_Stats.lowest_time_ns;
+    fprintf(stdout, "EDES was %lld times slower than DES\n", des_slowness);
 }
